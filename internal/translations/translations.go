99f98d86abf68442becface15940a32a6cd2e2fc
abakum <koka.abakum@gmail.com>
2025-12-05 12:05:22 +0300

    text

--- internal/translations/translations.go
+++ internal/translations/translations.go
@@ -1,5 +1,10 @@
 package translations
 
 //go install golang.org/x/text/cmd/gotext@latest
+//go install github.com/abakum/text/cmd/gotext@master
+
+//git clone https://github.com/abakum/text
+//cd text/cmd/gotext
+//go install
 
 //go:generate gotext -srclang=en-US update -out=catalog.go -lang=en-US,tr-TR,ja-JP,zh-CN,zh-HK,zh-TW,ru-RU crocgui
